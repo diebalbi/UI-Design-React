1bf5ecfd42e8c6f96674eb84cbd1f34250627f92
Pablo Pereira <invict1@outlook.es>
2020-04-20 09:39:05 -0300

    [Initial Commit] The project was created using a base provided by our professor.

---
+++ obligatorio/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo.svg";
+// import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
+
+import "./App.css";
+
+function App() {
+  return <RegisterForm />;
+}
+
+export default App;
